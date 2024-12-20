151d0f0019c6cd327366e68b2741650177a4d438
AdeibArief <sadeib3@gmail.com>
2024-12-20 17:09:58 +0530

    Fixed NLTK punkt resource issue

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # import streamlit as st
 # import pickle
 # st.title('Email spam classifier')
+
+
+
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt', download_dir='./nltk_data')
 
 ps = PorterStemmer()
 
