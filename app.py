b952941091b41431147b6f7cb9a14ac1fda56f01
AdeibArief <sadeib3@gmail.com>
2024-12-20 17:10:51 +0530

    Fixed NLTK punkt resource issue

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt', download_dir='./nltk_data')
+nltk.download('punkt_tab')
 
 ps = PorterStemmer()
 
